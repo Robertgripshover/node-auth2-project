649ed7d8118586c30e56c01b39c3f116d44e9305
Robert Gripshover <robertgrips@gmail.com>
2023-04-12 13:13:58 -0400

    m

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -48,7 +48,11 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
-    const { role_name } = req.decodedToken
+    if (role_name === req.decodedToken.role_name) {
+      next()
+    } else {
+      next({ status: 403, message: 'This is not for you'})
+    }
 }
 
 
